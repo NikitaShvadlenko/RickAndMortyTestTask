5361c26dbf458e47b3b20a84621d9c01c6c50630
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2023-08-18 18:55:47 +1200

    Initial commit

---
+++ RickAndMortyTestTask/AppDelegate.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = ViewController()
+        let mainScreenNavigationController = UINavigationController(
+            rootViewController: viewController
+        )
+        window.rootViewController = mainScreenNavigationController
+
+        self.window = window
+        window.makeKeyAndVisible()
+        return true
+    }
+}
