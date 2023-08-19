2f297e707dfdd1e5a887dd614a83b27e198cea31
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2023-08-19 19:31:37 +1200

    wip

--- RickAndMortyTestTask/CharacterList/CollectionView/CharacterCell.swift
+++ RickAndMortyTestTask/CharacterList/CollectionView/CharacterCell.swift
@@ -37,6 +37,7 @@ final class CharacterCell: UICollectionViewCell {
         setupView()
     }
 
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
